941ab98daf623b7e9b0fa743e78e05f99f70656e
Ulfflo <145294663+Ulfflo@users.noreply.github.com>
2024-09-20 17:34:16 +0200

    project update

--- app/page.js
+++ app/page.js
@@ -16,6 +16,12 @@ import { TbBrandVscode } from "react-icons/tb";
 import Card from "../components/Card";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState, useEffect } from "react";
+import arbetarbanken from "../public/arbetarbanken.jpg";
+import quiz from "../public/quiz.jpg";
+import zoombild from "../public/zoombild.jpg";
+import cookbook from "../public/cookbook.jpg";
+import fairy from "../public/fairy.jpg";
+import grodan from "../public/grodan.jpg";
 
 export default function Home() {
   const [show, setShow] = useState(false);
@@ -42,19 +48,18 @@ export default function Home() {
 
   return (
     <main id="home" className="dark:bg-gray-900 dark:text-white">
-      <header className="flex fixed w-full h-16 text-black bg-white justify-between items-center px-6 dark:text-white dark:bg-black">
+      <header className="flex fixed w-full h-16 text-black bg-white justify-between items-center px-4 md:px-6 dark:text-white dark:bg-black">
         <nav>
           <div>
             <button
               onClick={() => setShow(!show)}
               className="block bg-black text-yellow-50 border-none md:hidden cursor-pointer"
             >
-              {" "}
-              <GiHamburgerMenu />{" "}
+              <GiHamburgerMenu />
             </button>
           </div>
           <div className="hidden md:block">
-            <ul className="flex items-center gap-32 px-20 font-mono">
+            <ul className="flex items-center gap-6 md:gap-32 px-4 md:px-20 font-mono">
               <a href="#home">Home</a>
               <a href="#about">About</a>
               <a href="#tech">Tech Stack</a>
@@ -103,7 +108,7 @@ export default function Home() {
           </div>
         )}
 
-        <div className="flex items-center gap-6 px-6 ">
+        <div className="flex items-center gap-4 md:gap-6 px-4 md:px-6 ">
           <button onClick={toggleTheme}>
             {isDarkMode ? <MdLightMode /> : <MdDarkMode />}
           </button>
@@ -114,73 +119,139 @@ export default function Home() {
           >
             <FaGithub />
           </a>
-          <a>
+          <a
+            href="https://www.linkedin.com/in/ulf-flod%C3%A9n-139714327/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin />
           </a>
         </div>
       </header>
 
-      <div
-        id="about"
-        className="flex flex-col items-center justify-center mb-20 md:flex-row"
-      >
-        <div className="flex items-center justify-center md:pl-32 mt-36 md:w-1/2">
-          <p className="text-4xl font-serif text-blue-950 dark:text-white">
+      <div className="flex flex-col items-center justify-center mb-12 md:mb-40 md:flex-row">
+        <div className="flex items-center justify-center px-4 md:pl-32 mt-24 md:mt-36 md:w-1/2">
+          <p className="text-xl md:text-4xl font-serif text-blue-950 dark:text-white text-center md:text-left">
             My name is Ulf Flodén and I build things for the web
           </p>
         </div>
-        <div className="flex items-center justify-center mt-36 md:w-1/2">
+        <div className="flex items-center justify-center mt-12 md:mt-36 md:w-1/2">
           <Image
             src={Ulf}
-            className="placeholder:blur w-80 h-80 rounded-full border-4 border-solid"
+            className="placeholder:blur w-40 h-40 md:w-80 md:h-80 rounded-full border-4 border-solid"
             alt="Ulf's Picture"
           />
         </div>
       </div>
-      <div id="tech" className="mb-20">
-        <div className="flex flex-col text-center mb-16">
-          <h2 className="text-2xl text-blue-950 dark:text-white font-bold">
-            My Tech Stach
+
+      <div id="about" className="mb-12 md:mb-40 scroll-mt-32">
+        <div className="flex flex-col text-center mb-8 md:mb-16 mx-4 md:ml-60 md:mr-60">
+          <h2 className="text-xl md:text-2xl text-blue-950 dark:text-white font-bold mb-4 md:mb-10">
+            About me
           </h2>
-          <p>Technologies I've been working with recently</p>
+          <p className="text-base md:text-xl">
+            I’m a full-stack student with a strong emphasis on frontend
+            development. I have a passion for building engaging and intuitive
+            web and mobile applications. I love turning ideas into reality and
+            constantly seek new challenges to enhance my skills. Whether it's
+            crafting responsive layouts, optimizing user experiences or
+            developing mobile apps, I thrive on creating solutions that enhance
+            usability and engagement.
+          </p>
         </div>
-        <div id="projects">
-          <div className="flex justify-center gap-12 mb-12">
-            <FaHtml5 className="text-orange-500 w-20 h-20" />
-            <IoLogoCss3 className="text-blue-500 w-20 h-20" />
-            <IoLogoJavascript className="text-yellow-400 w-20 h-20" />
+      </div>
+
+      <div id="tech" className="mb-12 md:mb-40 scroll-mt-24">
+        <div className="flex flex-col text-center mb-8 md:mb-16">
+          <h2 className="text-xl md:text-2xl text-blue-950 dark:text-white font-bold mb-3">
+            My Tech Stack
+          </h2>
+          <p className="text-sm md:text-base">
+            Technologies I've been working with recently
+          </p>
+        </div>
+
+        <div>
+          <div className="flex justify-center gap-4 md:gap-12 mb-6 md:mb-12">
+            <FaHtml5 className="text-orange-500 w-12 h-12 md:w-20 md:h-20" />
+            <IoLogoCss3 className="text-blue-500 w-12 h-12 md:w-20 md:h-20" />
+            <IoLogoJavascript className="text-yellow-400 w-12 h-12 md:w-20 md:h-20" />
           </div>
-          <div className="flex justify-center gap-12">
-            <FaReact className="text-cyan-300 w-20 h-20" />
-            <SiTailwindcss className="text-cyan-500 w-20 h-20" />
-            <TbBrandVscode className="text-blue-600 w-20 h-20" />
-            <FaGithub className="w-20 h-20" />
+
+          <div className="flex justify-center gap-4 md:gap-12">
+            <FaReact className="text-cyan-300 w-12 h-12 md:w-20 md:h-20" />
+            <SiTailwindcss className="text-cyan-500 w-12 h-12 md:w-20 md:h-20" />
+            <TbBrandVscode className="text-blue-600 w-12 h-12 md:w-20 md:h-20" />
+            <FaGithub className="w-12 h-12 md:w-20 md:h-20" />
           </div>
         </div>
       </div>
+
       <div>
-        <div id="project" className="flex flex-col text-center mb-16">
-          <h2 className="text-2xl text-blue-950 dark:text-white font-bold">
+        <div id="projects" className="flex flex-col text-center mb-8 md:mb-16 scroll-mt-36">
+          <h2 className="text-xl md:text-2xl text-blue-950 dark:text-white font-bold mb-3">
             Projects
           </h2>
-          <p>Things I've built so far</p>
+          <p className="text-sm md:text-base">
+            Some of the things I've built so far
+          </p>
         </div>
+
         <div>
-          <div className="flex justify-center gap-16 mb-12">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-16 mb-8 md:mb-12">
             <Card
+              picture={quiz}
               title="Quiz"
-              description="A quiz made with Next.js and Redux"
+              description="A fun quiz, built with Next.js, Redux and Tailwind CSS."
               git="https://github.com/Ulfflo/quiz-redux"
               link="https://quiz-redux-rdziuy51p-ulf-flodens-projects.vercel.app/"
             />
-            <Card />
-            <Card />
+            <Card
+              picture={arbetarbanken}
+              title="Bank"
+              description="An application where you can deposit your money, built with Next.js, Express and Tailwind CSS."
+              git="https://github.com/Ulfflo/bank"
+            />
+            <Card
+              picture={zoombild}
+              title="Zoom"
+              description="An alternative design for Zoom, created with Vite and Tailwind CSS."
+              git="https://github.com/Ulfflo/zoom2"
+              link="https://zoom2-9kewox9k7-ulf-flodens-projects.vercel.app/"
+            />
           </div>
-          <div className="flex justify-center gap-16">
-            <Card />
-            <Card />
-            <Card />
+
+          <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-16">
+            <Card
+              picture={cookbook}
+              title="Cookbook"
+              description="Keep your recipes in this Cookbook, built with Reactive Native and Expo."
+              git="https://github.com/Ulfflo/recept-app"
+            />
+            <Card
+              picture={fairy}
+              title="FairyTailor"
+              description="Brings you AI-generated fairytales. Built with Next.js, Express and MySQL."
+              git="https://github.com/Ulfflo/FairyTailor"
+            />
+            <Card
+              picture={grodan}
+              title="Store"
+              description="Shop 'til you drop in this store, built with Next.js, Redux and Tailwind CSS."
+              git="https://github.com/Ulfflo/shop"
+              link="https://shop-rf25renvz-ulf-flodens-projects.vercel.app/"
+            />
           </div>
+        </div>
+      </div>
+
+      <div id="contact" className="mt-20 md:mt-40">
+        <div className="flex flex-col text-center mb-8 md:mb-16">
+          <h2 className="text-xl md:text-2xl text-blue-950 dark:text-white font-bold mb-3">
+            Contact
+          </h2>
+          <p className="text-sm md:text-base">phone: +46 760 185 007</p>
+          <p className="text-sm md:text-base">mail: ulf_floden@hotmail.com</p>
         </div>
       </div>
     </main>
--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,11 @@
-import colours from "../public/colours.jpg";
+
 import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 
-export default function Card({ title, description, git, link }) {
+export default function Card({ title, description, git, link, picture }) {
   return (
     <div className="border border-solid rounded-md w-48 h-72 flex flex-col">
-      <Image src={colours} className="rounded-t-md" />
+      <Image src={picture} className="rounded-t-md" />
       <div className="flex flex-col flex-grow">
         <div className="flex-grow p-4">
           <h2 className="text-lg font-bold text-blue-950 dark:text-white">
@@ -22,14 +22,16 @@ export default function Card({ title, description, git, link }) {
           >
             <FaGithub className="mr-2" />
           </a>
-          <a
-            href={link}
-            className="dark:text-white"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Go to site
-          </a>
+          {link && (
+            <a
+              href={link}
+              className="dark:text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go to site
+            </a>
+          )}
         </div>
       </div>
     </div>
